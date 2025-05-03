d28a90ff634500b652af6cc55195c21b7ae07ada
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2025-05-03 03:22:51 +0000

    follow system theme

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system, // Follow system theme
       home: const AuthCheckScreen(),
     );
   }
